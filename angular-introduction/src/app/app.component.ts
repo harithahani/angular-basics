722f4833534d3eba08ab6bb53704362798f4eed2
haritha <haritha@techjini.com>
2018-10-01 16:23:34 +0530

    component basics

--- angular-introduction/src/app/app.component.ts
+++ angular-introduction/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // selector property which assigns string as a value.
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3{
+    color:dodgerblue; 
+    }
+  `]
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  // title = 'my app'; // this value can be a dynamically added or can be any data from server.
 }
